073b7f7ef670d270f8e6eee3c871236016916b08
TaxiRadar24 <taxiradar24app@gmail.com>
2026-03-09 12:57:21 +0100

    Visual IU Shop

--- src/navigator/layouts/AcademyPublicLayout.js
+++ src/navigator/layouts/AcademyPublicLayout.js
@@ -1,14 +1,24 @@
 // src/navigator/layouts/AcademyPublicLayout.js
 
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import HeaderAcademia from "@/components/HeaderBox/HeaderAcademia";
 
 export default function AcademyPublicLayout() {
+
+  const { pathname } = useLocation();
+
+  const showHeader =
+    pathname.startsWith("/academia/demo") ||
+    pathname.startsWith("/academia/pro");
+
   return (
     <>
-      <HeaderAcademia />
-      <Outlet />
+      {showHeader && <HeaderAcademia />}
+
+      <main style={{ minHeight: "100vh" }}>
+        <Outlet />
+      </main>
     </>
   );
 }
